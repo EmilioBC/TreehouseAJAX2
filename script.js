5be68706d3a99a20f1b329575ffc25b8cb135182
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-10-31 01:16:41 +0000

    xhrPRACTICE

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if(xhr.readyState===4 && xhr.status===200) {
+//         document.getElementById('ajax').innerHTML = xhr.responseText;
+//     }
+// };
+// xhr.open('GET', 'sidebar.html');
+// xhr.send();
+
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
-    if(xhr.readyState===4 && xhr.status===200) {
+    if(xhr.readyState === 4 && xhr.status === 200) {
         document.getElementById('ajax').innerHTML = xhr.responseText;
     }
 };
-xhr.opent('GET', 'sidebar.html');
+xhr.open('GET', 'sidebar.html');
 xhr.send();
