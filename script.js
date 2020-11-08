d758cb23fdec954c3c6e92bf974c7967c873a778
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-11-08 05:02:13 +0000

    try

--- script.js
+++ script.js
@@ -5,13 +5,13 @@
 //     }
 // };
 // xhr.open('GET', 'sidebar.html');
-// xhr.send();
+// xhr.send()
 
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    xhr.readyState === 4 && xhr.status === 200 {
-        document.getElementById('ajax').innerHTML = xhr.responseText;
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200) {
+        document.getElementById('ajax').innerHTML =xhr.responseText;
     }
 };
-xhr.open('GET','sidebar.HTML');
+xhr.open('GET','sidebar.html');
 xhr.send();
