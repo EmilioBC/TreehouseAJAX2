ab9b2baea0b37bfc3a54cbe8242287577cd4ff2e
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-11-09 06:08:19 +0000

    practice

--- script.js
+++ script.js
@@ -8,9 +8,9 @@
 // xhr.send()
 
 var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById('ajax').innerHTML =xhr.responseText;
+xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4) {
+        document.getElementById('sidebar').innerHTML = xhr.responseText;
     }
 };
 xhr.open('GET','sidebar.html');
